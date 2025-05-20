b6a3aecc5460040b58bda2497bf9652422ad2b05
Adriel Medeiros <130292427+Athelstan0420@users.noreply.github.com>
2025-05-20 11:03:37 -0300

    Update enviar_email.py

--- enviar_email.py
+++ enviar_email.py
@@ -5,6 +5,7 @@ from email.mime.multipart import MIMEMultipart #Criar email com várias partes, 
 from email.mime.text import MIMEText # Para colocar os textos dentros dos emails
 import requests
 from datetime import *
+import os
 
 
 #==========================================================================================
@@ -16,15 +17,15 @@ requisicao = requests.get(api)
 requisicao_dic = requisicao.json()
 cot_bitcoin = requisicao_dic["BTCBRL"]["bid"]
 
-# data_atual = datetime.now()
-# data_em_texto = datetime.strftime(data_atual, "%d/%m/%Y %H:%M") # Pega uma data/hora e transforma em texto;
+data_atual = datetime.now()
+data_em_texto = datetime.strftime(data_atual, "%d/%m/%Y %H:%M") # Pega uma data/hora e transforma em texto;
 
 
-# import pytz #Python timezone => tornar qqr data ciente do local dela. 
-# # para saber qual sigla utilizar pesquise: "https://en.wikipedia.org/wiki/List_of_tz_database_time_zones"
-# fuso_horario = pytz.timezone("America/Sao_Paulo") #UTC -> padrão 00h
-# minha_data_com_fuso = fuso_horario.localize(data_atual) #dada minhda posição de greenwich
-# conv_txt = str(minha_data_com_fuso)
+import pytz #Python timezone => tornar qqr data ciente do local dela. 
+# para saber qual sigla utilizar pesquise: "https://en.wikipedia.org/wiki/List_of_tz_database_time_zones"
+fuso_horario = pytz.timezone("America/Sao_Paulo") #UTC -> padrão 00h
+minha_data_com_fuso = fuso_horario.localize(data_atual) #dada minhda posição de greenwich
+conv_txt = str(minha_data_com_fuso)
 
 texto = f"Cotação atualizada!\n\nData: \n\nValor da cotação: R$ {cot_bitcoin},00"
 
