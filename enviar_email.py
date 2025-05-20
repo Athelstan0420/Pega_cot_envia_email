9ae1a01a19d3b33f34ece18cb8de41302040b039
Adriel Medeiros <130292427+Athelstan0420@users.noreply.github.com>
2025-05-20 11:00:13 -0300

    Update enviar_email.py

--- enviar_email.py
+++ enviar_email.py
@@ -16,19 +16,18 @@ requisicao = requests.get(api)
 requisicao_dic = requisicao.json()
 cot_bitcoin = requisicao_dic["BTCBRL"]["bid"]
 
-data_atual = datetime.now()
-data_em_texto = datetime.strftime(data_atual, "%d/%m/%Y %H:%M") # Pega uma data/hora e transforma em texto;
+# data_atual = datetime.now()
+# data_em_texto = datetime.strftime(data_atual, "%d/%m/%Y %H:%M") # Pega uma data/hora e transforma em texto;
 
 
-import pytz #Python timezone => tornar qqr data ciente do local dela. 
-# para saber qual sigla utilizar pesquise: "https://en.wikipedia.org/wiki/List_of_tz_database_time_zones"
-fuso_horario = pytz.timezone("America/Sao_Paulo") #UTC -> padrão 00h
-minha_data_com_fuso = fuso_horario.localize(data_atual) #dada minhda posição de greenwich
-conv_txt = str(minha_data_com_fuso)
+# import pytz #Python timezone => tornar qqr data ciente do local dela. 
+# # para saber qual sigla utilizar pesquise: "https://en.wikipedia.org/wiki/List_of_tz_database_time_zones"
+# fuso_horario = pytz.timezone("America/Sao_Paulo") #UTC -> padrão 00h
+# minha_data_com_fuso = fuso_horario.localize(data_atual) #dada minhda posição de greenwich
+# conv_txt = str(minha_data_com_fuso)
 
-texto = f"Cotação atualizada!\n\nData: {conv_txt[:19]} - {conv_txt[-5:]} \n\nValor da cotação: R$ {cot_bitcoin},00"
+texto = f"Cotação atualizada!\n\nData: \n\nValor da cotação: R$ {cot_bitcoin},00"
 
-# print(texto)
 
 #==========================================================================================
 
